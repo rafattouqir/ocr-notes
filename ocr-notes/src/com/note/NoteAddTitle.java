bc7fd57ebc2d0b9fbb2282b675c96d8f96422b58
aim.knight17@gmail.com <aim.knight17@gmail.com@3c258a0c-6703-767e-f4dd-635a9350fb1d>
2011-09-07 07:42:49 +0000

    added add title dialog

---
+++ ocr-notes/src/com/note/NoteAddTitle.java
@@ -0,0 +1,31 @@
+package com.note;
+
+import com.database.DatabaseAdapter;
+
+import keendy.projects.R;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NoteAddTitle extends Dialog{
+
+
+	
+	public NoteAddTitle(Context context) {
+		super(context);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.title_dialog);
+	}
+
+	
+
+	
+
+	
+}
