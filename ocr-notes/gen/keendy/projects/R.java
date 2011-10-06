e017856fa2906eb37751ee66d0fb4399929c6490
akiwarheit@gmail.com <akiwarheit@gmail.com@3c258a0c-6703-767e-f4dd-635a9350fb1d>
2011-10-06 17:19:51 +0000

    Added db4o to build path

--- ocr-notes/gen/keendy/projects/R.java
+++ ocr-notes/gen/keendy/projects/R.java
@@ -10,55 +10,73 @@ package keendy.projects;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int Black=0x7f040001;
+        public static final int Orange=0x7f040000;
+        public static final int White=0x7f040002;
+    }
     public static final class drawable {
         public static final int about=0x7f020000;
         public static final int about02=0x7f020001;
         public static final int about03=0x7f020002;
-        public static final int bulletin=0x7f020003;
-        public static final int camera=0x7f020004;
-        public static final int header=0x7f020005;
-        public static final int ic_logo_dudenotes=0x7f020006;
-        public static final int ic_menu_about=0x7f020007;
-        public static final int ic_menu_bulletin=0x7f020008;
-        public static final int ic_menu_camera=0x7f020009;
-        public static final int ic_menu_notebook=0x7f02000a;
-        public static final int ic_menu_textnote=0x7f02000b;
-        public static final int ic_menu_writenote=0x7f02000c;
-        public static final int icon=0x7f02000d;
-        public static final int menu_background_firstrow=0x7f02000e;
-        public static final int menu_background_secondrow=0x7f02000f;
-        public static final int menu_background_secondrow_white=0x7f020010;
-        public static final int menu_background_thirdrow=0x7f020011;
-        public static final int menu_background_thirdrow_white=0x7f020012;
-        public static final int menu_bg_01=0x7f020013;
-        public static final int notebook=0x7f020014;
-        public static final int text_note01=0x7f020015;
+        public static final int allnotes=0x7f020003;
+        public static final int allnotes2=0x7f020004;
+        public static final int allnotes3=0x7f020005;
+        public static final int bulletin=0x7f020006;
+        public static final int camera=0x7f020007;
+        public static final int createnote3=0x7f020008;
+        public static final int doodle3=0x7f020009;
+        public static final int doodle4=0x7f02000a;
+        public static final int header=0x7f02000b;
+        public static final int ic_logo_dudenotes=0x7f02000c;
+        public static final int ic_menu_about=0x7f02000d;
+        public static final int ic_menu_bulletin=0x7f02000e;
+        public static final int ic_menu_camera=0x7f02000f;
+        public static final int ic_menu_notebook=0x7f020010;
+        public static final int ic_menu_textnote=0x7f020011;
+        public static final int ic_menu_writenote=0x7f020012;
+        public static final int icon=0x7f020013;
+        public static final int menu_background_firstrow=0x7f020014;
+        public static final int menu_background_secondrow=0x7f020015;
+        public static final int menu_background_secondrow_white=0x7f020016;
+        public static final int menu_background_thirdrow=0x7f020017;
+        public static final int menu_background_thirdrow_white=0x7f020018;
+        public static final int menu_bg_01=0x7f020019;
+        public static final int notebook=0x7f02001a;
+        public static final int notebook2=0x7f02001b;
+        public static final int pluglogo=0x7f02001c;
+        public static final int tagnote3=0x7f02001d;
+        public static final int text_note01=0x7f02001e;
     }
     public static final class id {
-        public static final int BottomRow=0x7f050014;
-        public static final int MiddleFirst=0x7f05000a;
-        public static final int MiddleFirstCol=0x7f05000e;
-        public static final int MiddleSecond=0x7f05000d;
-        public static final int MiddleSecondCol=0x7f050011;
-        public static final int TopRow=0x7f050007;
-        public static final int about_content=0x7f050000;
-        public static final int camera_button=0x7f050006;
-        public static final int camera_linear_layout_button=0x7f050005;
-        public static final int camera_linear_layout_label=0x7f050003;
-        public static final int camera_relative_layout=0x7f050001;
-        public static final int camera_surface=0x7f050002;
-        public static final int home_about=0x7f050013;
-        public static final int home_bulletin=0x7f05000c;
-        public static final int home_captureNote=0x7f05000f;
-        public static final int home_myNotebook=0x7f05000b;
-        public static final int home_textNote=0x7f050012;
-        public static final int home_userName=0x7f050009;
-        public static final int home_writeNote=0x7f050010;
-        public static final int logo=0x7f050008;
-        public static final int note=0x7f050015;
-        public static final int notes_title_list=0x7f050016;
-        public static final int textView1=0x7f050004;
-        public static final int web_title_list=0x7f050017;
+        public static final int BottomRow=0x7f07000e;
+        public static final int MiddleFirstCol=0x7f07000b;
+        public static final int MiddleSecond=0x7f07000a;
+        public static final int MiddleSecondCol=0x7f070011;
+        public static final int TopRow=0x7f070007;
+        public static final int about_content=0x7f070000;
+        public static final int add_bttn=0x7f070018;
+        public static final int add_menu=0x7f07001a;
+        public static final int add_title=0x7f070017;
+        public static final int camera_button=0x7f070006;
+        public static final int camera_linear_layout_button=0x7f070005;
+        public static final int camera_linear_layout_label=0x7f070003;
+        public static final int camera_relative_layout=0x7f070001;
+        public static final int camera_surface=0x7f070002;
+        public static final int cancel_menu=0x7f07001b;
+        public static final int home_about=0x7f070013;
+        public static final int home_bulletin=0x7f070010;
+        public static final int home_captureNote=0x7f07000c;
+        public static final int home_myNotebook=0x7f07000f;
+        public static final int home_textNote=0x7f070012;
+        public static final int home_userName=0x7f070009;
+        public static final int home_writeNote=0x7f07000d;
+        public static final int image_title_dialog=0x7f070016;
+        public static final int logo=0x7f070008;
+        public static final int note=0x7f070014;
+        public static final int notes_title_list=0x7f070015;
+        public static final int textView1=0x7f070004;
+        public static final int web_title_list=0x7f070019;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -67,15 +85,19 @@ public final class R {
         public static final int list_tab=0x7f030003;
         public static final int note_editor=0x7f030004;
         public static final int notes_list=0x7f030005;
-        public static final int web_list=0x7f030006;
+        public static final int title_dialog=0x7f030006;
+        public static final int web_list=0x7f030007;
+    }
+    public static final class menu {
+        public static final int title_menu=0x7f060000;
     }
     public static final class string {
-        public static final int about_label=0x7f040003;
-        public static final int about_text=0x7f040004;
-        public static final int app_name=0x7f040000;
-        public static final int camera_button_label=0x7f040005;
-        public static final int camera_label=0x7f040006;
-        public static final int search_label=0x7f040002;
-        public static final int snap_label=0x7f040001;
+        public static final int about_label=0x7f050003;
+        public static final int about_text=0x7f050004;
+        public static final int app_name=0x7f050000;
+        public static final int camera_button_label=0x7f050005;
+        public static final int camera_label=0x7f050006;
+        public static final int search_label=0x7f050002;
+        public static final int snap_label=0x7f050001;
     }
 }
