062d52343459e9ccc1347d5cdde921d960d2cf83
akiwarheit@gmail.com <akiwarheit@gmail.com@3c258a0c-6703-767e-f4dd-635a9350fb1d>
2011-10-11 06:45:48 +0000

    Added more fields to match SQL model in web server.

--- ocr-notes/src/com/plug/database/models/Note.java
+++ ocr-notes/src/com/plug/database/models/Note.java
@@ -1,13 +1,37 @@
 package com.plug.database.models;
 
+import android.text.format.Time;
+
 import com.google.gson.annotations.SerializedName;
 
 public class Note {
 
-	@SerializedName("id") 			private long id;
-	@SerializedName("title") 		private String title;
-	@SerializedName("content") 	private String content;
-		
+	@SerializedName("id") 								private long id;
+	@SerializedName("title") 							private String title;
+	@SerializedName("content") 						private String content;
+	@SerializedName("created_at") 				private Time created;
+	@SerializedName("updated_at")					private Time updated;	
+	
+	private boolean isUploaded = false;
+	
+	public Note(String title, String content, Time created, Time updated) {
+	  this.title = title;
+	  this.content = content;
+	  this.created = created;
+	  this.updated = updated;
+  }
+
+	public Note() {
+  }
+
+	public Note(long id, String title, String content, Time created, Time updated) {
+	  this.id = id;
+	  this.title = title;
+	  this.content = content;
+	  this.created = created;
+	  this.updated = updated;
+  }
+
 	private String getTitle() {
 		return title;
 	}
@@ -31,5 +55,28 @@ public class Note {
 	private void setId(long id) {
 		this.id = id;
 	}
+
+	public boolean isUploaded() {
+  	return isUploaded;
+  }
+
+	public void setUploaded(boolean isUploaded) {
+  	this.isUploaded = isUploaded;
+  }
 	
+	public void setCreated(Time created) {
+		this.created = created;
+	}
+	
+	public Time getCreated() {
+		return created;
+	}
+	
+	public void setUpdated(Time updated) {
+		this.updated = updated;
+	}
+	
+	public Time getUpdated() {
+		return updated;
+	}
 }
